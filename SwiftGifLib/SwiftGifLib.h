d29f4fa6d811bc52da44322eb801d5f54293001f
Daniel Poulsen <DanP@daniels-macbook-pro-2.local>
2016-09-19 11:48:11 +0100

    added a dynamic lib for carthage

---
+++ SwiftGifLib/SwiftGifLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGifLib.h
+//  SwiftGifLib
+//
+//  Created by Daniel Poulsen on 19/09/2016.
+//  Copyright © 2016 alexiscreuzot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGifLib.
+FOUNDATION_EXPORT double SwiftGifLibVersionNumber;
+
+//! Project version string for SwiftGifLib.
+FOUNDATION_EXPORT const unsigned char SwiftGifLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGifLib/PublicHeader.h>
+
+
